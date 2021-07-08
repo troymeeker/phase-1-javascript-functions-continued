be9ff4dbdd30f139204529b61ec1d4582fba85e9
troymeeker <troymeeker1@gmail.com>
2021-07-08 13:28:06 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function saturdayFun(activity= "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+
+function mondayWork(work='go to the office'){
+ return (`This Monday, I will ${work}.`)
+}
+
+
+function wrapAdjective(value="*"){
+    
+    return function(adjective="special"){
+     const string = "You are";
+   
+        return(`${string} ${value}${adjective}${value}!`);
+    
+    }
+}
